aeabd9494bfa887af0be5e422ca27bdeba4b3a2d
Inna Enns <meerenns@gmail.com>
2020-05-17 23:11:54 -0700

    add positive and negative tests for applications
    - Adds positive and negative tests for endpoint: /applications/{id}

--- tests/param_api_test.py
+++ tests/param_api_test.py
@@ -11,6 +11,14 @@ class MyPositionsTest(unittest.TestCase):
         ('letters', 'abc', 500, 'ER_BAD_FIELD_ERROR'),
         ('space', ' ', 204, '')
     ]
+    applications_id_input_pos = [
+        ('happy path', 2, 200, 'Principal Automation Engineer')
+    ]
+    applications_id_input_neg = [
+        ('application does not exist', 222, 400, 'Incorrect applicationId: 222'),
+        ('letters', 'abc', 500, 'ER_BAD_FIELD_ERROR'),
+        ('space', ' ', 400, 'Incorrect applicationId: 0')
+    ]
 
     def setUp(self) -> None:
         self.base_url = 'https://recruit-portnov.herokuapp.com/recruit/api/v1'
@@ -27,6 +35,23 @@ class MyPositionsTest(unittest.TestCase):
 
         self.authorization_header = {'Authorization': 'Bearer ' + token}
         self.authorization_header.update(self.headers)
+
+    @parameterized.expand(applications_id_input_pos)
+    def test_get_applications_id_pos(self, test_name, application_id, status_code, title):
+        my_applications = requests.get(self.base_url + '/applications/' + str(application_id))
+        self.assertEqual(status_code, my_applications.status_code)
+        json_my_applications = json.loads(my_applications.text)
+        self.assertEqual(json_my_applications['title'], title)
+
+    @parameterized.expand(applications_id_input_neg)
+    def test_get_applications_id_neg(self, test_name, application_id, status_code, error_message):
+        my_applications = requests.get(self.base_url + '/applications/' + str(application_id))
+        self.assertEqual(status_code, my_applications.status_code)
+        json_my_applications = json.loads(my_applications.text)
+        if my_applications.status_code == 500:
+            self.assertEqual(json_my_applications['code'], error_message)
+        else:
+            self.assertEqual(json_my_applications['errorMessage'], error_message)
 
     @parameterized.expand(positions_input)
     def test_get_candidate_positions(self, test_name, user_id, status_code, error_message):

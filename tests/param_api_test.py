bb65958fffc4cde10230bae365d4ad62b0428263
Inna Enns <meerenns@gmail.com>
2020-05-17 21:50:46 -0700

    fix position tests

--- tests/param_api_test.py
+++ tests/param_api_test.py
@@ -1,15 +1,14 @@
 import json
 import unittest
 
-import parameterized as parameterized
 import requests
 from parameterized import parameterized
 
 
 class MyPositionsTest(unittest.TestCase):
-    alist = [
+    positions_input = [
         ('user does not exist', 9999, 204, ''),
-        ('letters', 'abc', 200, 'ER_BAD_FIELD_ERROR'),
+        ('letters', 'abc', 500, 'ER_BAD_FIELD_ERROR'),
         ('space', ' ', 204, '')
     ]
 
@@ -29,16 +28,17 @@ class MyPositionsTest(unittest.TestCase):
         self.authorization_header = {'Authorization': 'Bearer ' + token}
         self.authorization_header.update(self.headers)
 
-    @parameterized.expand(alist)
+    @parameterized.expand(positions_input)
     def test_get_candidate_positions(self, test_name, user_id, status_code, error_message):
         my_positions = self.get_candidate_positions(user_id)
         self.assertEqual(status_code, my_positions.status_code)
-        json_my_positions = json.loads(my_positions.text)
+        if my_positions.status_code != 204:
+            json_my_positions = json.loads(my_positions.text)
+            self.assertEqual(json_my_positions['code'], error_message)
 
     def get_candidate_positions(self, user_id):
         return requests.get(self.base_url + '/candidates/' + str(user_id) + '/positions',
                             headers=self.authorization_header)
-    # self.assertEqual(json_my_positions['code'], error_message)
 
     def login(self, email, password):
         return requests.post(self.base_url + '/login', json={"email": email, "password": password})

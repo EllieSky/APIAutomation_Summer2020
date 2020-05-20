a2964a48dfcef74b9a2e0bc05c55e68e1a002b05
soniya <ssinghal22@yahoo.com>
2020-05-20 11:16:04 -0700

    HW2 by EY, class 3

--- tests/my_test.py
+++ tests/my_test.py
@@ -2,6 +2,9 @@ import json
 import unittest
 import requests
 
+from lib.authentication import Authenticate
+
+
 class YahooAPITestCase(unittest.TestCase):
     def test_for_successful_response(self):
         result = requests.get("http://www.yahoo.com")
@@ -11,56 +14,34 @@ class YahooAPITestCase(unittest.TestCase):
 
 class CareerPortalTests(unittest.TestCase):
     def setUp(self) -> None:
-        self.base_url = 'https://recruit-portnov.herokuapp.com/recruit/api/v1'
-        self.headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
-
-    def get_all_positions(self):
-        return requests.get(self.base_url + '/positions')
-
-    def authenticate(self, aemail, apassword):
-        return requests.post(self.base_url + '/login', json={"email": aemail, "password": apassword})
-
-    def perform_user_verification(self, token):
-        verify_header = {'Authorization': 'Bearer ' + token}
-        verify_header.update(self.headers)
-        return requests.post(self.base_url + '/verify', headers=verify_header)
-
-    def get_candidate_positions(self, user_id):
-        return requests.get(self.base_url + '/candidates/' + str(user_id) + '/positions')
-
-    def get_application(self, id):
-        return requests.get(self.base_url + '/applications/' + str(id))
+        pass
 
     def test_login(self):
-        positions = self.get_all_positions()
+        sess = Authenticate() #sess is an object of class, creates instance of class
+        positions = sess.get_all_positions()
         json_positions = json.loads(positions.text)
         self.assertEqual(5, len(json_positions))
 
-        application = self.get_application(1)
+        application = sess.get_application(1)
         json_application = json.loads(application.text)
-        self.assertEqual(aid, json_application['id'])
+        self.assertEqual(1, json_application['id'])
 
-        result = self.authenticate('student@example.com', 'welcome')
+        result = sess.authenticate('student@example.com', 'welcome')
         self.assertEqual(200, result.status_code)
 
-        json_parsed = json.loads(result.text)
-        self.assertTrue(json_parsed['authenticated'])
-
-        token = json_parsed['token']
-
-        verify_response = self.perform_user_verification(token)
+        verify_response = sess.perform_user_verification()
         verify_content = json.loads(verify_response.content)
 
         user_id = verify_content['id']
         email = verify_content['email']
 
-        self.assertTrue(email == aemail)
+        self.assertTrue('student@example.com', email)
         self.assertEqual(8, user_id)
 
-        my_positions = self.get_candidate_positions(user_id)
+        my_positions = sess.get_candidate_positions(user_id)
         json_my_positions = json.loads(my_positions.text)
 
-        self.assertEqual(len(json_my_positions), 3)
+        self.assertEqual(1, len(json_my_positions))
 
 
 if __name__ == '__main__':

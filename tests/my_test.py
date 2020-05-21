7a75d04cf7bec465bd492413fc65c2b3480ebf34
Elena <742042@gmail.com>
2020-05-21 12:51:28 -0700

    homework_2

--- tests/my_test.py
+++ tests/my_test.py
@@ -7,23 +7,22 @@ from lib.recruit_career.rc_client import RecruitClient
 
 
 class YahooAPITestCase(unittest.TestCase):
-    def test_for_successful_response(self):
+    def test_for_successful_response (self):
         result = requests.get("http://www.yahoo.com")
         self.assertEqual(200, result.status_code)
         # OR
         self.assertTrue('OK' == result.reason)
 
 
-
 class CareerPortalTests(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp (self) -> None:
         pass
 
-    def test_login(self):
+    def test_login (self):
         client = RecruitClient()
         # client.authentication.authenticate('jane@example.com', 'pass')
 
-        # # if you wanter parallel clients
+        # # if you want parallel clients
         # client2 = RecruitClient()
         # client2.authentication.authenticate('bob@example.com', 'Boo')
         #
@@ -43,26 +42,28 @@ class CareerPortalTests(unittest.TestCase):
         self.assertGreaterEqual(len(json_positions), 5)
 
         result = client.authentication.authenticate("student@example.com", "welcome")
+
+        sess = Authenticate()
+        # authenticate/login/POST
+        result = sess.authenticate("student@example.com", "welcome")
+
         self.assertEqual(200, result.status_code)
 
         json_parsed = json.loads(result.text)
         self.assertTrue(json_parsed['authenticated'])
 
         verify_response = client.authentication.perform_user_verification()
+
         verify_content = json.loads(verify_response.content)
 
+        # verify user_ID
         user_id = verify_content['id']
         email = verify_content['email']
 
         self.assertTrue(email == 'student@example.com')
         self.assertEqual(8, user_id)
 
-        my_positions = client.candidate.get_candidate_positions(user_id)
-        json_my_positions = json.loads(my_positions.text)
-
-        self.assertLessEqual(1, len(json_my_positions))
-
-    def test_cannot_login(self):
+    def test_cannot_login (self):
         sess = Authenticate()
         response = sess.authenticate('foo', 'barr')
         json_parsed = json.loads(response.text)

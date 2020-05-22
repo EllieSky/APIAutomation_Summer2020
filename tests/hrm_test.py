3b1c4f2ccf6362074fc0a0c417ff15921d1fb8c6
Elena <742042@gmail.com>
2020-05-21 22:48:59 -0700

    kk

--- tests/hrm_test.py
+++ tests/hrm_test.py
@@ -1,18 +1,9 @@
 import os
-import time
-import random
 import unittest
 import bs4
-<<<<<<< HEAD
-=======
-from requests import Session
-from  faker import Faker
->>>>>>> hrm
-
 from requests import Session
 from faker import Faker
 from requests_toolbelt import MultipartEncoder
-
 from lib.hrm.steps import HRM
 
 
@@ -31,10 +22,6 @@ class HRMTest(unittest.TestCase):
         # OR
         self.assertTrue(resp.url.endswith('/pim/viewEmployeeList'))
 
-        emp_number = str(time.time()).split('.')[-1]
-            # OR
-        emp_number = random.randrange(100000, 999999)
-            # OR
         f = self.f
 
         first_name = f.first_name()
@@ -71,8 +58,7 @@ class HRMTest(unittest.TestCase):
 
         self.assertIn('/recruitment/addCandidate/id/', resp.url)
 
-<<<<<<< HEAD
-=======
+
 class HRMTestCase(unittest.TestCase):
     def setUpClass(self) -> None:
         self.url = 'http://hrm-online.portnov.com/symfony/web/index.php/auth'
@@ -80,30 +66,14 @@ class HRMTestCase(unittest.TestCase):
         self.sess = Session()
         self.sess.headers.update({'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'})
 
-    def test_create_employee(self):
-        login_url = '/login'
-        resp = self.sess.get(self.url + login_url)
-        soup = bs4.BeautifulSoup(resp.content, 'html5lib')
-        result = soup.find('input', attr = {'name': '_csrf_token'})
-        token = result['value']
-
-        authenticate_url = 'validateCredentials'
-
-        login_data = {
-            '_csrf_token': token,
-            'txtUsername': 'admin',
-            'txtPassword': 'password'
-        }
-
-        resp = self.sess.post(self.url + authenticate_url, data= login_data)
 
 
-        self.assertIn('viewEmployeeLis', resp.url)
 
-        f = Faker
-        first_name = f.first_name()
-        last_name = f.last_name()
->>>>>>> hrm
+
+
+
+
+
 
 
 if __name__ == '__main__':

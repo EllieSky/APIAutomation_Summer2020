f6acbf0d2cc38225b5b351b38723fcecd51144f3
Elena <742042@gmail.com>
2020-05-28 15:01:21 -0700

    homework3, optional step: to check a candidate data

--- tests/hrm_test.py
+++ tests/hrm_test.py
@@ -7,14 +7,13 @@ f = Faker()
 
 
 class HRMTest(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp (self) -> None:
         self.url = "http://hrm-online.portnov.com/symfony/web/index.php"
-        self.hrm = HrmAuthenticate()
-
+        self.sess = HrmAuthenticate()
 
     def test_create_employee (self):
         # Step 1: get the landing page - contains login form
-        response = self.hrm.login('admin', 'password')
+        response = self.sess.login('admin', 'password')
         self.assertTrue(response.ok)
         self.assertIn('/pim/viewEmployeeList', response.url)
         # OR
@@ -22,16 +21,20 @@ class HRMTest(unittest.TestCase):
 
         # Step 4:  get the add employee page  - contains the FORM to add employee
         emp_id = f.random_number(6)
-        resp = self.hrm.add_employee(emp_id)
+        resp = self.sess.add_employee(emp_id)
         self.assertIn('/pim/viewPersonalDetails/empNumber', resp.url)
 
-        # Optional step, to check that data posted correctly
-        # resp = self.hrm.session(resp.url)
-        # soup = bs4.BeautifulSoup(resp.content, 'html5lib')
-        # actual_emp_id = soup.select_one('#personal_txtEmployeeId')['value']
-
-        # self.assertEqual(str(emp_id), actual_emp_id)
+        # # Optional step, to check that data posted correctly with verification
+        # same_employee = self.sess.verify_candidate_data(resp.url)
+        # self.assertTrue(same_employee)
 
 
-    if __name__ == '__main__':
-        unittest.main()
+     # Optional step, to check that data posted correctly
+        resp = self.sess.verify_candidate_data(resp.url)
+        soup = bs4.BeautifulSoup(resp.content, 'html5lib')
+        actual_emp_id = soup.select_one('#personal_txtEmployeeId')['value']
+        self.assertEqual(str(emp_id), actual_emp_id)
+
+
+if __name__ == '__main__':
+    unittest.main()

7ad58588204e40b56c54012d6dfc7a964efd1e42
Inna Enns <meerenns@gmail.com>
2020-05-28 13:44:30 -0700

    candidate was successfully added and that all data was added correctly
    - Part 1.3

--- lib/hrm_online.py
+++ lib/hrm_online.py
@@ -1,4 +1,7 @@
+from io import StringIO
+
 import bs4
+from dateutil.utils import today
 from faker import Faker
 from requests import Session
 
@@ -14,12 +17,6 @@ class HrmOnline(object):
         )
         self.faker = Faker()
 
-    def extract_emp_id(self, resp):
-        resp = self.sess.get(resp.url)
-        soup = bs4.BeautifulSoup(resp.content, 'html5lib')
-        actual_emp_id = soup.select_one('#personal_txtEmployeeId')['value']
-        return actual_emp_id
-
     def login(self, username, password):
         login_uri = "/auth/login"
         resp = self.sess.get(self.url + login_uri)
@@ -34,20 +31,37 @@ class HrmOnline(object):
         return resp
 
     def add_emp(self, emp_data):
-        add_emp_uri = "/pim/addEmployee"
-        resp = self.sess.get(self.url + add_emp_uri)
+        uri = '/pim/addEmployee'
+        resp = self.sess.get(self.url + uri)
         token = self.extract_token(resp)
         emp_data['_csrf_token'] = token
-        resp = self.sess.post(self.url + add_emp_uri, data=emp_data)
+        resp = self.sess.post(self.url + uri, data=emp_data)
+        return resp
+
+    def extract_emp_id(self, resp):
+        resp = self.sess.get(resp.url)
+        soup = bs4.BeautifulSoup(resp.content, 'html5lib')
+        actual_emp_id = soup.select_one('#personal_txtEmployeeId')['value']
+        return actual_emp_id
+
+    def add_candidate(self, cand_data):
+        uri = '/recruitment/addCandidate'
+        resp = self.sess.get(self.url + uri)
+        token = self.extract_token(resp, 'addCandidate[_csrf_token]')
+        cand_json = cand_data.copy()
+        cand_json['addCandidate[_csrf_token]'] = token
+        resp = self.sess.post(self.url + uri, files={'addCandidate[resume]': ('', StringIO(''), 'application/octet'
+                                                                                                '-stream')},
+                              data=cand_json)
         return resp
 
     @staticmethod
-    def extract_token(resp):
+    def extract_token(resp, name='_csrf_token'):
         soup = bs4.BeautifulSoup(resp.content, 'html5lib')
-        result = soup.find('input', attrs={'name': '_csrf_token'})
+        result = soup.find('input', attrs={'name': name})
         return result['value']
 
-    def generate_emp_json(self):
+    def generate_emp_data(self):
         first_name = self.faker.first_name()
         last_name = self.faker.last_name()
         emp_id = self.faker.random_number(6)
@@ -57,3 +71,40 @@ class HrmOnline(object):
             'employeeId': emp_id,
         }
         return emp_data
+
+    def generate_cand_data(self):
+        first_name = self.faker.first_name()
+        last_name = self.faker.last_name()
+        email = self.faker.email()
+        cand_data = {
+            'addCandidate[firstName]': first_name,
+            'addCandidate[middleName]': '',
+            'addCandidate[lastName]': last_name,
+            'addCandidate[email]': email,
+            'addCandidate[contactNo]': '',
+            'addCandidate[vacancy]': '',
+            'addCandidate[keyWords]': '',
+            'addCandidate[comment]': '',
+            'addCandidate[appliedDate]': today().strftime('%m-%d-%Y')
+        }
+        return cand_data
+
+    @staticmethod
+    def extract_cand_data(resp):
+        bs = bs4.BeautifulSoup(resp.content, 'html5lib')
+        cand_data = {
+            'addCandidate[firstName]': get_input(bs, 'addCandidate[firstName]'),
+            'addCandidate[middleName]': get_input(bs, 'addCandidate[middleName]'),
+            'addCandidate[lastName]': get_input(bs, 'addCandidate[lastName]'),
+            'addCandidate[email]': get_input(bs, 'addCandidate[email]'),
+            'addCandidate[contactNo]': get_input(bs, 'addCandidate[contactNo]'),
+            'addCandidate[keyWords]': get_input(bs, 'addCandidate[keyWords]'),
+            'addCandidate[appliedDate]': get_input(bs, 'addCandidate[appliedDate]'),
+            'addCandidate[comment]': bs.find(id='addCandidate_comment').text,
+            'addCandidate[vacancy]': bs.find('option', attrs={'selected': 'selected'}).get('value', '')
+        }
+        return cand_data
+
+
+def get_input(soup, name):
+    return soup.find('input', attrs={'name': name})['value']
--- tests/hrm_test.py
+++ tests/hrm_test.py
@@ -11,12 +11,23 @@ class HRMTest(unittest.TestCase):
         resp = self.sess.login('admin', 'password')
         self.assertTrue(resp.url.endswith('/pim/viewEmployeeList'))
 
-        emp_data = self.sess.generate_emp_json()
+        emp_data = self.sess.generate_emp_data()
         resp = self.sess.add_emp(emp_data)
         self.assertIn('/pim/viewPersonalDetails/empNumber', resp.url)
 
         actual_emp_id = self.sess.extract_emp_id(resp)
         self.assertEqual(str(emp_data['employeeId']), actual_emp_id)
+
+    def test_create_candidate(self):
+        resp = self.sess.login('admin', 'password')
+        self.assertTrue(resp.url.endswith('/pim/viewEmployeeList'))
+
+        cand_data = self.sess.generate_cand_data()
+        resp = self.sess.add_candidate(cand_data)
+        self.assertIn('/recruitment/addCandidate/id/', resp.url)
+
+        actual_cand_data = self.sess.extract_cand_data(resp)
+        self.assertEqual(cand_data, actual_cand_data)
 
 
 if __name__ == '__main__':

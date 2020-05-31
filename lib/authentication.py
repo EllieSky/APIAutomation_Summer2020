4d0e3cf69b0bfc8ca8b39b2fce84dfba27cb7159
Elena <742042@gmail.com>
2020-05-31 12:16:59 -0700

    m

--- lib/authentication.py
+++ lib/authentication.py
@@ -1,6 +1,6 @@
 import json
 
-from lib.base import BaseRequest
+from lib.base import BaseClient
 
 
 class Authenticate(BaseClient):
@@ -13,10 +13,6 @@ class Authenticate(BaseClient):
             self.session.headers.update({'Authorization': 'Bearer ' + token})
         return resp
 
+
     def perform_user_verification(self):
         return self.session.post(self.base_url + '/verify')
-
-# goal: token will be part of the session all the time we don't need to pass token to the func
-# all functions need to be in 1 container, that session can be shared,all function can use it.
-# We create new instance of this container it create new instance of the session and all
-# funct-s can use it)
